b92e46bddc816a02bc92bc0cea8a0f2335607342
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2022-05-16 16:10:12 +0100

    test: ensure SignUpController returns an error if no name is provided

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
-export default {
+module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '.+\\.ts$': 'ts-jest'
   }
 }

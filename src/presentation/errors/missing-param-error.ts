d518fa91704d289e200d640eba1599a8b5b5f33c
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2022-05-17 17:03:10 +0100

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/errors/missing-param-error.ts
+++ src/presentation/errors/missing-param-error.ts
@@ -4,10 +4,3 @@ export class MissingParamError extends Error {
     this.name = 'MissingParamError'
   }
 }
-
-export class InvalidParamError extends Error {
-  constructor (paramName: string) {
-    super(`Invalid param: ${paramName}`)
-    this.name = 'InvalidParamError'
-  }
-}

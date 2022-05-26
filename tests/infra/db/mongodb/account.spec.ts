78e90c00e5198a10fe6233ed6c3813059b4f8cc7
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2022-05-26 16:58:03 +0100

    test: ensure AccountMongoRepository returns an account on success

---
+++ tests/infra/db/mongodb/account.spec.ts
@@ -0,0 +1,35 @@
+// import { Collection } from 'mongodb'
+import { Collection } from 'mongodb'
+import { AccountMongoRepository } from '../../../../src/infra/db/mongodb/account'
+import { MongoHelper } from '../../../../src/infra/db/mongodb/mongo-helper'
+
+let accountCollection: Collection
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+})
